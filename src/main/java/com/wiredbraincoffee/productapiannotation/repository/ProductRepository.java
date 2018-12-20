cdd578c45f8dda111481f98fd2dc3bd85409b585
viktorija <tosheva.victoria@yahoo.com>
2018-12-20 22:39:41 +0100

    first commit

---
+++ src/main/java/com/wiredbraincoffee/productapiannotation/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.wiredbraincoffee.productapiannotation.repository;
+
+import com.wiredbraincoffee.productapiannotation.model.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+}
